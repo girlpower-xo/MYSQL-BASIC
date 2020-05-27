2d15129c7e4bb473c03f64f554bc959c4e6f955e
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-27 11:58:25 +0200

    2-taak 1 af

--- 2-Select/taak01/antwoorden.sql
+++ 2-Select/taak01/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROm jaar2015
 -- Opdracht 3
-
+.....
 -- Opdracht 4
-
+SELECT region FROM jaar2015
 -- Opdracht 5
-
+SELECT region , score FROM jaar2015
 -- Opdracht 6
-
+SELECT rank, score from jaar2016
 -- Opdracht 7
